634c3567e421816f2d3794acbad2e543389c4924
Josue <jderazo4@espe.edu.ec>
2019-08-16 12:54:01 -0500

    Agregado un ejemplo sencillo de busqueda

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template lang="pug">
   #app
-    img(alt="Vue logo" src="./assets/logo.png")
-    h1 {{ msg }} 
-    h2 papito ric 
+    section.section
+      nav.nav.has-shadow
+        .field.has-addons
+          input.input.is-large(
+            type="text" 
+            placeholder="Buscar canciones" 
+            v-model="searchQuery")
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times
+      .container.custom
+        .colums
+          .colum(v-for= "t in canciones") {{ t.nombre }} - {{ t.artista }}
+          
+          
 </template>
 
 <script>
+const canciones = [
+  { nombre: "De mes en mes", artista: "Ricardo Arjona" },
+  { nombre: "Pinguinos en la cama", artista: "Ricardo Arjona" },
+  { nombre: "Atrevete", artista: "Calle 13" },
+  { nombre: "Nadie como tu", artista: "Calle 13" }
+];
 export default {
   name: "app",
   data() {
     return {
-      msg: "Hello Josue"
+      canciones: [],
+      searchQuery: ""
     };
+  },
+  methods: {
+    search() {
+      this.canciones = canciones;
+    }
   }
 };
 </script>
