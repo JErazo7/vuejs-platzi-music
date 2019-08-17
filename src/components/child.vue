47830a89a0614e0f8c07d54528e87e84ac2f20fa
Josue <jderazo4@espe.edu.ec>
2019-08-17 13:09:39 -0500

    Ejemplo de como usar componentes locales y globales

---
+++ src/components/child.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  div
+    h1 Este es un componente hijo
+    local
+</template>
+
+<script>
+import local from "./local.vue";
+export default {
+  components: {
+    local
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
+import child from "./components/child.vue";
 
 Vue.config.productionTip = false;
+Vue.component("child", child);
 
 new Vue({
   render: h => h(App)
