6f52c327f3f993c582667b8282463bf579a44c6a
Josue <jderazo4@espe.edu.ec>
2019-08-27 19:02:46 -0500

    Se soluciono el marco verde de selccion en la cancion

--- src/mixins/track.js
+++ src/mixins/track.js
@@ -4,6 +4,7 @@ const trackMixin = {
       if (!this.track.preview_url) {
         return;
       }
+      this.$emit("select", this.track.id);
       this.$store.commit("setTrack", this.track);
     }
   }
